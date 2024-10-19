e7d9a9154103661d91ae2ff1ebfdc28063459653
Sharukh <sharukhashfaq64@gmail.com>
2024-10-19 19:21:11 +0530

    new feature added

--- NebulaChessEngine/Src/Core/Board.h
+++ NebulaChessEngine/Src/Core/Board.h
@@ -25,27 +25,6 @@ namespace NCE {
 			m_pieces[WHITEKNIGHT].Print();
 		}
 
-		static inline uint64_t PositionToBitBoard(const int n) {
-			return ((uint64_t)1 << n);
-		}
-
-		static inline uint64_t PositionToBitBoard(const int n1, const int n2) {
-			return ((uint64_t)1 << n1) | ((uint64_t)1 << n2);
-		}
-
-		static inline uint64_t nthRank(const int n) {
-			uint64_t firstRank = PositionToBitBoard(a1) | PositionToBitBoard(b1) | PositionToBitBoard(c1) | PositionToBitBoard(d1)
-				| PositionToBitBoard(e1) | PositionToBitBoard(f1) | PositionToBitBoard(g1) | PositionToBitBoard(h1);
-			return (firstRank << (8 * (n - 1)));
-		}
-
-		static inline uint64_t nthFile(const int n) {
-			return PositionToBitBoard(a1 + (n - 1)) | PositionToBitBoard(a2 + (n - 1))
-				| PositionToBitBoard(a3 + (n - 1)) | PositionToBitBoard(a4 + (n - 1))
-				| PositionToBitBoard(a5 + (n - 1)) | PositionToBitBoard(a6 + (n - 1))
-				| PositionToBitBoard(a7 + (n - 1)) | PositionToBitBoard(a8 + (n - 1));
-		}
-
 		void Print() const;
 
 	private:
